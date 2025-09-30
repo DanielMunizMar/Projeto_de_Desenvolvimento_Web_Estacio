ffbe9c28b57fb38caa3d21d57727de8a5d962374
Brunoosteixeiira_ <brunosousa75@outlook.com>
2025-09-30 08:57:38 -0300

    Alterações no CSS da barra de navegação com centralização no centro dos links das páginas de navegação criação de um arquivo em JS para adesão de uma logo clicavel que retorna a página inicial do site e referenciação do arquivo em JS em todas as páginas do site.

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+//Imagem da logo quando clicada retorna a página inicial do site.
+
+document.getElementById("logo").addEventListener("click", () => {
+window.location.href = "../../index.html"
+});
