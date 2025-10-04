c8c24098ffbca63d132b8a17baad86b831cdf54f
Gabriel Henrique <henriquedeveloper24@gmail.com>
2025-10-04 18:35:57 -0300

    Colocando trabalheconosco.js no arquivo index.js e adicionando ponteiro do mouse no icone da barra de navegação

--- src/js/index.js
+++ src/js/index.js
@@ -4,4 +4,18 @@ document.getElementById("logo").addEventListener("click", () => {
 window.location.href = "../../index.html"
 });
 
+// Quando colocar o começo das palavras que tem no título da vaga, o sistema filtra e mostra apenas as vagas que contém aquelas palavras.
+document.getElementById("btnPesquisa").addEventListener("click", () => {
+    const query = document.getElementById("pesquisa").value.toLowerCase();
+    const items = document.querySelectorAll(".vaga-descricao");
+
+    items.forEach(item => {
+        const title = item.querySelector("h4").textContent.toLowerCase();
+        if (title.includes(query)) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+})
 
