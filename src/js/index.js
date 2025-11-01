30bdcf9a6d8349ae93c65821b294c27d546a6aa3
DanielMunizMar <daniel05muniz@gmail.com>
2025-11-01 17:11:33 -0300

    js responsivo

--- src/js/index.js
+++ src/js/index.js
@@ -73,3 +73,16 @@ botoesDescricao.forEach((botao) => {
     mostrarDetalhesDaVaga(vagaId);
   });
 });
+
+//RESPONSIVIDADE DO HEADER
+function menuShow(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "../images/icons/aberto.svg";
+    }else{
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "../images/icons/fechado.svg";
+    }
+}
+//FIM DA RESPONSIVIDADE DO HEADER
